b60421c6fed9df269f708915f3ebf10c511eddaf
Jumamwinga <160024251+Jumamwinga@users.noreply.github.com>
2024-02-26 13:13:40 +0300

    Add files via upload

---
+++ Use_Input.py
@@ -0,0 +1,7 @@
+Name = input ("What is you name?:")
+Age = int (input ("How old are you?:"))
+Location = input ("Where are you located?:")
+
+print ("Hello" + Name)
+print ("You are" + str(Age) + "years old")
+print ("and live in" + Location)
